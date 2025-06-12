1307f5f50871f08640b3eb93eeb5c48929cc3844
kaizenacademy <kaizen.techacademy@gmail.com>
2025-06-12 01:47:34 +0000

    class2 files

---
+++ class2/task2.py
@@ -0,0 +1,12 @@
+tip=int(input("Enter tip: "))
+
+if tip == 15:
+    print("standard")
+elif tip == 18:
+    print("good")
+elif tip == 20:
+    print("great")
+elif tip > 20:
+    print("my hero")
+else:
+    print("wrong number")
